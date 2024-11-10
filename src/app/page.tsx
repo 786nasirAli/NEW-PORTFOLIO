d9b9f5ab162e6a27c2d648bd5434256125bba09b
sikandar Ali <160599968+786nasirAli@users.noreply.github.com>
2024-11-10 02:33:06 -0800

    new revisions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import styles from './page.module.css';
 
  const Home = () => {
@@ -7,15 +6,15 @@ import styles from './page.module.css';
     <div>
        <section className={styles.home}>
       <div className={styles.intro}>
-        <h1 className='text-3xl'>Hello, I'm Sikandar Ali</h1>
+        <h1 className='text-3xl'>Hello, I&lsquo;m Sikandar Ali</h1>
         <p className='text-1xl'>
-        Hello! I'm a passionate web developer and IT student currently pursuing a course in Cloud Applied Generative AI Engineering. With a solid foundation in HTML, CSS, JavaScript/TypeScript, and frameworks like Next.js and React, I enjoy creating engaging and visually appealing user interfaces. I have successfully completed multiple projects focusing on modern design principles, responsiveness, and user experience.
+        Hello! I&lsquo;m a passionate web developer and IT student currently pursuing a course in Cloud Applied Generative AI Engineering. With a solid foundation in HTML, CSS, JavaScript/TypeScript, and frameworks like Next.js and React, I enjoy creating engaging and visually appealing user interfaces. I have successfully completed multiple projects focusing on modern design principles, responsiveness, and user experience.
         </p>
-        <p>I am also diving into data science and artificial intelligence to expand my skill set further. My goal is to develop innovative solutions that leverage the power of AI and cloud technology. As I grow in this field, I'm excited to work on projects that challenge me and help me learn new technologies.</p>
+        <p>I am also diving into data science and artificial intelligence to expand my skill set further. My goal is to develop innovative solutions that leverage the power of AI and cloud technology. As I grow in this field, I&lsquo;m excited to work on projects that challenge me and help me learn new technologies.</p>
       </div>
       <div className={styles.imagediv}>
       <div className={styles.image} >
-        <img src="/nasir portfolio.jpg" alt="Profile Image " />
+        <Image src="/nasir portfolio.jpg" alt="Profile Image" width={500} height={500}/>
         
 
 
