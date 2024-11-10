e1e4d77dbfa75285a4c6657f979be448f17a523f
sikandar Ali <160599968+786nasirAli@users.noreply.github.com>
2024-11-10 02:09:12 -0800

    Add package.json file

--- cd/src/app/page.tsx
+++ cd/src/app/page.tsx
@@ -12,7 +12,6 @@ import styles from './page.module.css';
         Hello! I'm a passionate web developer and IT student currently pursuing a course in Cloud Applied Generative AI Engineering. With a solid foundation in HTML, CSS, JavaScript/TypeScript, and frameworks like Next.js and React, I enjoy creating engaging and visually appealing user interfaces. I have successfully completed multiple projects focusing on modern design principles, responsiveness, and user experience.
         </p>
         <p>I am also diving into data science and artificial intelligence to expand my skill set further. My goal is to develop innovative solutions that leverage the power of AI and cloud technology. As I grow in this field, I'm excited to work on projects that challenge me and help me learn new technologies.</p>
-        <Link href="/contact" className={styles.button}>Contact me</Link>
       </div>
       <div className={styles.imagediv}>
       <div className={styles.image} >
